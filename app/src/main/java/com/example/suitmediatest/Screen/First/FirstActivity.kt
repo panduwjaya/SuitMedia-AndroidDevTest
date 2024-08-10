4e3bb9c5e828ccca85846376fa43ea95a6a3b593
panduwjaya <wijayapandu388@gmail.com>
2024-08-11 00:29:55 +0700

    Initial commit

---
+++ app/src/main/java/com/example/suitmediatest/Screen/First/FirstActivity.kt
@@ -0,0 +1,106 @@
+package com.example.suitmediatest.Screen.First
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.suitmediatest.R
+import com.example.suitmediatest.Screen.Second.SecondActivity
+import com.example.suitmediatest.databinding.ActivityFirstBinding
+import com.google.android.material.textfield.TextInputEditText
+
+class FirstActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityFirstBinding
+
+    private lateinit var btnNext: Button
+    private lateinit var btnCheck: Button
+    private lateinit var inputName: TextInputEditText
+    private lateinit var inputPaindrome: TextInputEditText
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityFirstBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initComponents()
+        setupListener()
+    }
+
+    private fun initComponents() {
+        inputName = binding.inputName
+        inputPaindrome = binding.inputPalindrome
+
+        btnNext = binding.btnNext
+        btnCheck = binding.btnCheckPalindrome
+    }
+
+    private fun setupListener() {
+        var checked = false
+        var username = ""
+        var palindrome = ""
+
+        btnCheck.setOnClickListener {
+            var message = ""
+            username = inputName.text.toString()
+            palindrome = inputPaindrome.text.toString()
+
+            if (palindrome.isEmpty()) {
+                message = "Field palindrome cannot be empty"
+            } else if (palindrome.isNotEmpty() && !isPalindrome(palindrome)) {
+                message = "Not Palindrome"
+                checked = false
+            } else {
+                message = "Is Palindrome"
+                checked = true
+            }
+            showDialogMessage(message)
+        }
+
+        btnNext.setOnClickListener {
+            username = inputName.text.toString()
+            palindrome = inputPaindrome.text.toString()
+
+            if (username.isEmpty() && palindrome.isEmpty()) {
+                showDialogMessage("Field cannot be empty")
+            } else if (username.isEmpty() && checked) {
+                showDialogMessage("Username cannot be empty")
+            } else if (username.isEmpty() && !checked) {
+                showDialogMessage("Check username and palindrome")
+            } else if (username.isNotEmpty() && !checked) {
+                showDialogMessage("Check palindrome first")
+            } else if (username.isNotEmpty() && palindrome.isEmpty()) {
+                showDialogMessage("Field palindrome cannot be empty")
+            } else if (username.isNotEmpty() && palindrome.isNotEmpty() && !checked) {
+                showDialogMessage("Check palindrome first")
+            } else {
+                inputName.text?.clear()
+                inputPaindrome.text?.clear()
+
+                val intent = Intent(this, SecondActivity::class.java)
+                intent.putExtra("username", username)
+                startActivity(intent)
+                finish()
+            }
+        }
+    }
+
+    private fun isPalindrome(value: String): Boolean {
+        val text = value.replace("\\s".toRegex(), "").lowercase()
+        return text == text.reversed()
+    }
+
+    private fun showDialogMessage(message: String) {
+        val alertDialogBuilder = AlertDialog.Builder(this)
+        alertDialogBuilder.setTitle("Palindrome Check")
+        alertDialogBuilder.setMessage(message)
+        alertDialogBuilder.setPositiveButton("OK") { dialog, _ -> dialog.dismiss() }
+
+        val alertDialog = alertDialogBuilder.create()
+        alertDialog.show()
+    }
+}
---
+++ app/src/main/java/com/example/suitmediatest/Screen/Second/SecondActivity.kt
@@ -0,0 +1,62 @@
+package com.example.suitmediatest.Screen.Second
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.example.suitmediatest.Screen.First.FirstActivity
+import com.example.suitmediatest.Screen.Third.ThirdActivity
+import com.example.suitmediatest.databinding.ActivitySecondBinding
+
+class SecondActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySecondBinding
+
+    private lateinit var username: TextView
+    private lateinit var selectedUser: TextView
+    private lateinit var btnBack: ImageButton
+    private lateinit var btnChooseUser: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySecondBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initComponents()
+        setupComponents()
+        setupListener()
+    }
+
+    private fun initComponents() {
+        username = binding.name
+        btnBack = binding.btnBack
+        selectedUser = binding.selectedUser
+        btnChooseUser = binding.btnChooseUser
+    }
+
+    private fun setupComponents() {
+        val username = intent.getStringExtra("username")
+        val selectedUser = intent.getStringExtra("selected_username")
+
+        this.username.text = username ?: "Guest"
+        this.selectedUser.text = selectedUser ?: "Selected User Name"
+    }
+
+    private fun setupListener() {
+        val username = intent.getStringExtra("username")
+        val selectedUsername = intent.getStringExtra("selected_username")
+
+        btnBack.setOnClickListener {
+            startActivity(Intent(this, FirstActivity::class.java))
+        }
+
+        btnChooseUser.setOnClickListener {
+            val intent = Intent(this, ThirdActivity::class.java)
+            intent.putExtra("username", username)
+            intent.putExtra("selected_username", selectedUsername)
+            startActivity(intent)
+        }
+    }
+}
